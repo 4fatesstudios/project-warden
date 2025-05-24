d9e364b6e0cef7893e5c4b585d8a95575c022ec4
Chris Liwanag <143138753+cliwanag07@users.noreply.github.com>
2025-05-23 22:03:05 -0700

    init character and initiator

---
+++ Assets/Code/Character/Character.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+/// <summary>
+/// Basic character class
+/// </summary>
+public class Character : MonoBehaviour {
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Code/GameInitiator.cs
@@ -0,0 +1,19 @@
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class GameInitiator : MonoBehaviour
+{
+    private async void Start() {
+        // BindObjects();
+        // _loadingScreen.Show();
+        // await InitializeObjects();
+        // await CreateObjects();
+        // PrepareGame();
+        // _loadingScreen.Hide();
+    }
+
+    private void BindObjects() {
+        
+    }
+    
+}
