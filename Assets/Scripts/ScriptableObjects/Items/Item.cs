afe49e4c612160f3b0d782c3e1b57d14532afb87
Chris Liwanag <143138753+cliwanag07@users.noreply.github.com>
2025-05-31 16:34:14 -0700

    item attributes

--- Assets/Scripts/ScriptableObjects/Items/Item.cs
+++ Assets/Scripts/ScriptableObjects/Items/Item.cs
@@ -1,10 +1,34 @@
+using System;
+using UnityEngine;
+
 namespace FourFatesStudios.ProjectWarden.ScriptableObjects.Items
 {
-    using UnityEngine;
-    
-    
-    public abstract class Item : ScriptableObject {
-        
+    public enum ItemRarity {
+        None,
+        Common,
+        Rare,
+        Epic,
+        Mythic,
+        Unique
     }
     
+    public abstract class Item : ScriptableObject {
+        [SerializeField] private string itemName;
+        [SerializeField] private string itemDescription;
+        private string _itemID;
+        [SerializeField] private ItemRarity itemRarity;
+
+        public string ItemName => itemName;
+        public string ItemDescription => itemDescription;
+        public string ItemID => _itemID;
+        public ItemRarity ItemRarity => itemRarity;
+        
+#if UNITY_EDITOR
+        private void OnValidate() {
+            if (string.IsNullOrEmpty(_itemID)) {
+                _itemID = Guid.NewGuid().ToString();
+            }
+        }
+#endif
+    }
 }
