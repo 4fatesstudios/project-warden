f29c511f81161ef3421e86034c71d9d4eed693b6
Chris Liwanag <143138753+cliwanag07@users.noreply.github.com>
2025-05-31 17:38:27 -0700

    item list and lookup added

--- Assets/Scripts/ScriptableObjects/Databases/ItemDatabase.cs
+++ Assets/Scripts/ScriptableObjects/Databases/ItemDatabase.cs
@@ -1,10 +1,36 @@
+using System.Collections.Generic;
+using FourFatesStudios.ProjectWarden.ScriptableObjects.Items;
 using UnityEngine;
 
 
 namespace FourFatesStudios.ProjectWarden.ScriptableObjects.Databases
 {
-    public class ItemDatabase : MonoBehaviour
-    {
+    [CreateAssetMenu(fileName = "NewDatabase", menuName = "Databases/Item Database")]
+    public class ItemDatabase : ScriptableObject {
+        [SerializeField] private List<Item> items = new();
+
+        private Dictionary<string, Item> itemLookup;
         
+        public IReadOnlyList<Item> Items => items;
+
+        private void OnEnable() {
+            BuildLookup();
+        }
+
+        private void BuildLookup() {
+            itemLookup = new Dictionary<string, Item>();
+            foreach (var item in items) {
+                if (item != null && !string.IsNullOrEmpty(item.ItemID)) 
+                    itemLookup[item.ItemID] = item;
+            }
+        }
+
+        public Item GetItemById(string itemID) {
+            if (itemLookup == null)
+                BuildLookup();
+            
+            itemLookup.TryGetValue(itemID, out var item);
+            return item;
+        }
     }
 }
