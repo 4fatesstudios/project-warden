ece6dbce94898358f19bc929e3b47f3e083fe6c8
Chris Liwanag <143138753+cliwanag07@users.noreply.github.com>
2025-05-31 17:12:19 -0700

    added item ID regenerator & test generic item

--- Assets/Scripts/ScriptableObjects/Items/Item.cs
+++ Assets/Scripts/ScriptableObjects/Items/Item.cs
@@ -7,23 +7,34 @@ using UnityEditor;
 
 namespace FourFatesStudios.ProjectWarden.ScriptableObjects.Items
 {
-    
-    
-    public abstract class Item : ScriptableObject {
-        [SerializeField] private string itemName;
-        [SerializeField] private string itemDescription;
+    [CreateAssetMenu(fileName = "NewItem", menuName = "Items/Generic Item")]
+    public class Item : ScriptableObject
+    {
+        [SerializeField, Tooltip("Display name shown to the player.")]
+        private string itemName;
+
+        [SerializeField, Tooltip("Description shown to the player.")]
+        private string itemDescription;
+
         [SerializeField] private ItemRarity itemRarity;
+
         [SerializeField, HideInInspector] private string _itemID;
 
         public string ItemName => itemName;
         public string ItemDescription => itemDescription;
         public ItemRarity ItemRarity => itemRarity;
         public string ItemID => _itemID;
-        
+
 #if UNITY_EDITOR
-        private void OnValidate() {
-            if (string.IsNullOrEmpty(_itemID)) {
+        [ContextMenu("Regenerate Item ID")]
+        public void RegenerateID() => _itemID = Guid.NewGuid().ToString();
+
+        private void OnValidate()
+        {
+            if (string.IsNullOrEmpty(_itemID))
+            {
                 _itemID = Guid.NewGuid().ToString();
+                EditorUtility.SetDirty(this);
             }
         }
 #endif
