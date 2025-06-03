d503f777cc18030bdb49f63d60aef2f654626188
Chris Liwanag <143138753+cliwanag07@users.noreply.github.com>
2025-06-02 18:49:06 -0700

    key item init

--- Assets/Scripts/Characters/Components/InventoryComponent.cs
+++ Assets/Scripts/Characters/Components/InventoryComponent.cs
@@ -10,15 +10,18 @@ namespace FourFatesStudios.ProjectWarden.Characters.Components
         private List<Trinket> trinkets;
         private List<Potion> potions;
         private List<Ingredient> ingredients;
+        private List<KeyItem> keyItems;
         
         public IReadOnlyList<Trinket> Trinkets => trinkets.AsReadOnly();
         public IReadOnlyList<Potion> Potions => potions.AsReadOnly();
         public IReadOnlyList<Ingredient> Ingredients => ingredients.AsReadOnly();
+        public IReadOnlyList<KeyItem> KeyItems => keyItems.AsReadOnly();
 
         public void Awake() {
             trinkets = new List<Trinket>();
             potions = new List<Potion>();
             ingredients = new List<Ingredient>();
+            keyItems = new List<KeyItem>();
         }
     }
 }
