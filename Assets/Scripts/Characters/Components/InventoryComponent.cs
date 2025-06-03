42ac89d19bee3a52b589a7bef57a5f0bd4dadcb5
Chris Liwanag <143138753+cliwanag07@users.noreply.github.com>
2025-06-02 18:54:05 -0700

    recipe added

--- Assets/Scripts/Characters/Components/InventoryComponent.cs
+++ Assets/Scripts/Characters/Components/InventoryComponent.cs
@@ -12,12 +12,14 @@ namespace FourFatesStudios.ProjectWarden.Characters.Components
         private List<Ingredient> ingredients;
         private List<KeyItem> keyItems;
         private List<Note> notes;
+        private List<Recipe> recipes;
         
         public IReadOnlyList<Trinket> Trinkets => trinkets.AsReadOnly();
         public IReadOnlyList<Potion> Potions => potions.AsReadOnly();
         public IReadOnlyList<Ingredient> Ingredients => ingredients.AsReadOnly();
         public IReadOnlyList<KeyItem> KeyItems => keyItems.AsReadOnly();
         public IReadOnlyList<Note> Notes => notes.AsReadOnly();
+        public IReadOnlyList<Recipe> Recipes => recipes.AsReadOnly();
 
         public void Awake() {
             trinkets = new List<Trinket>();
@@ -25,6 +27,7 @@ namespace FourFatesStudios.ProjectWarden.Characters.Components
             ingredients = new List<Ingredient>();
             keyItems = new List<KeyItem>();
             notes = new List<Note>();
+            recipes = new List<Recipe>();
         }
     }
 }
