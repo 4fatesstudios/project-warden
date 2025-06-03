0cb1dbd7726e84dcbf1604815d5cf4c6c68fbf24
Chris Liwanag <143138753+cliwanag07@users.noreply.github.com>
2025-06-02 19:28:38 -0700

    PW-71-PW-72 (#14)
    * ingredients added to inventory component
    
    * key item init
    
    * PW-70-PW-71-end (#16)
    
    * notes item added
    
    * recipe added

---
+++ Assets/Scripts/ScriptableObjects/Items/Recipe.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+
+namespace FourFatesStudios.ProjectWarden.ScriptableObjects.Items
+{
+    [CreateAssetMenu(fileName = "NewRecipe", menuName = "Items/Recipe")]
+    public class Recipe : Item
+    {
+        
+    }
+}
