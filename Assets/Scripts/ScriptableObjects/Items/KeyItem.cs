b0dbafef151e88ca9ae305b6878e02423e49d237
Chris Liwanag <143138753+cliwanag07@users.noreply.github.com>
2025-06-02 19:29:20 -0700

    PW-72-PW-73 (#13)
    * enums & init ingredient
    
    * separated corruption from archetype and made it binary
    
    * example ingredient
    
    * PW-71-PW-72 (#14)
    
    * ingredients added to inventory component
    
    * key item init
    
    * PW-70-PW-71-end (#16)
    
    * notes item added
    
    * recipe added

---
+++ Assets/Scripts/ScriptableObjects/Items/KeyItem.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+
+namespace FourFatesStudios.ProjectWarden.ScriptableObjects.Items
+{
+    [CreateAssetMenu(fileName = "NewKeyItem", menuName = "Items/Key Item")]
+    public class KeyItem : Item
+    {
+        
+    }
+}
