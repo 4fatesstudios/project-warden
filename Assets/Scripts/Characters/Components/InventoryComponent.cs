1f57690a173ecb9f08157008133d97558b62ee75
Chris Liwanag <143138753+cliwanag07@users.noreply.github.com>
2025-06-02 17:28:38 -0700

    ingredients added to inventory component

--- Assets/Scripts/Characters/Components/InventoryComponent.cs
+++ Assets/Scripts/Characters/Components/InventoryComponent.cs
@@ -9,13 +9,16 @@ namespace FourFatesStudios.ProjectWarden.Characters.Components
     {
         private List<Trinket> trinkets;
         private List<Potion> potions;
+        private List<Ingredient> ingredients;
         
         public IReadOnlyList<Trinket> Trinkets => trinkets.AsReadOnly();
         public IReadOnlyList<Potion> Potions => potions.AsReadOnly();
+        public IReadOnlyList<Ingredient> Ingredients => ingredients.AsReadOnly();
 
         public void Awake() {
             trinkets = new List<Trinket>();
             potions = new List<Potion>();
+            ingredients = new List<Ingredient>();
         }
     }
 }
