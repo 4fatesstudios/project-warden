3aed497f5c01c6484db9512f16a6f1ed7f41206c
Chris Liwanag <143138753+cliwanag07@users.noreply.github.com>
2025-06-03 18:06:08 -0700

    definitions

--- Assets/Scripts/Interfaces/IInteract.cs
+++ Assets/Scripts/Interfaces/IInteract.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 namespace FourFatesStudios.ProjectWarden.Interfaces
 {
     public interface IInteract {
-        
+        void Interact();
+        string InteractText { get; }
     }
 }
