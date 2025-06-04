f4fc9243cf2d06b64a2494ae83100b61a82909e6
Alope396 <Alope396@ucr.edu>
2025-06-04 02:09:51 -0700

    Combat UI

--- Assets/Scripts/Characters/Controllers/PlayerController.cs
+++ Assets/Scripts/Characters/Controllers/PlayerController.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR;
 
 namespace FourFatesStudios.ProjectWarden.Characters.Controllers
 {
@@ -8,6 +9,17 @@ namespace FourFatesStudios.ProjectWarden.Characters.Controllers
     public class PlayerController : MonoBehaviour
     {
         private PlayerInput _playerInput;
+
+        public static event Action OnOpenSkills;
+        public static event Action OnOpenItems;
+        public static event Action OnGuard;
+        public static event Action OnAttack;
+        public static event Action OnCycleUp;
+        public static event Action OnCycleDown;
+        public static event Action OnCycleLeft;
+        public static event Action OnCycleRight;
+        public static event Action OnSelect;
+        public static event Action OnBack;
         
         #region Exploration Callbacks
         public static event Action OnExplorationInteractAction;
@@ -23,6 +35,51 @@ namespace FourFatesStudios.ProjectWarden.Characters.Controllers
             movementVector = movementVector.normalized;
             return movementVector;
         }
+
+        #region Combat Mapping
+
+        private void Combat_OpenSkills_performed(InputAction.CallbackContext obj){
+            OnOpenSkills?.Invoke();
+        }
+        
+        private void Combat_OpenItems_performed(InputAction.CallbackContext obj){
+            OnOpenItems?.Invoke();
+        }
+        
+        private void Combat_Guard_performed(InputAction.CallbackContext obj){
+            OnGuard?.Invoke();
+        }
+        
+        private void Combat_Attack_performed(InputAction.CallbackContext obj){
+            OnAttack?.Invoke();
+        }
+        
+        private void Combat_CycleUp_performed(InputAction.CallbackContext obj){
+            OnCycleUp?.Invoke();
+        }
+        
+        private void Combat_CycleDown_performed(InputAction.CallbackContext obj){
+            OnCycleDown?.Invoke();
+        }
+        
+        private void Combat_CycleLeft_performed(InputAction.CallbackContext obj){
+            OnCycleLeft?.Invoke();
+        }
+        
+        private void Combat_CycleRight_performed(InputAction.CallbackContext obj){
+            OnCycleRight?.Invoke();
+        }
+        
+        private void Combat_Select_performed(InputAction.CallbackContext obj){
+            OnSelect?.Invoke();
+        }
+        
+        private void Combat_Back_performed(InputAction.CallbackContext obj){
+            OnBack?.Invoke();
+        }
+        
+        #endregion
+        
         
         #region Exploration Mapping
         private void Exploration_Interact_performed(InputAction.CallbackContext obj) {
@@ -36,10 +93,35 @@ namespace FourFatesStudios.ProjectWarden.Characters.Controllers
             
             // Exploration Mapping
             _playerInput.PlayerInputMap_EXPLORATION.Interact.performed += Exploration_Interact_performed;
+            
+            //Combat Mapping
+            _playerInput.PlayerInputMap_COMBAT.OpenSkills.performed += Combat_OpenSkills_performed;
+            _playerInput.PlayerInputMap_COMBAT.OpenItems.performed += Combat_OpenItems_performed;
+            _playerInput.PlayerInputMap_COMBAT.Guard.performed += Combat_Guard_performed;
+            _playerInput.PlayerInputMap_COMBAT.Attack.performed += Combat_Attack_performed;
+            _playerInput.PlayerInputMap_COMBAT.CycleUp.performed += Combat_CycleUp_performed;
+            _playerInput.PlayerInputMap_COMBAT.CycleDown.performed += Combat_CycleDown_performed;
+            _playerInput.PlayerInputMap_COMBAT.CycleLeft.performed += Combat_CycleLeft_performed;
+            _playerInput.PlayerInputMap_COMBAT.CycleRight.performed += Combat_CycleRight_performed;
+            _playerInput.PlayerInputMap_COMBAT.Select.performed += Combat_Select_performed;
+            _playerInput.PlayerInputMap_COMBAT.Back.performed += Combat_Back_performed;
         }
-
-
+    
         private void OnDisable() {
+            _playerInput.PlayerInputMap_EXPLORATION.Disable();
+            _playerInput.PlayerInputMap_COMBAT.Disable();
+            
+            //Combat Mapping
+            _playerInput.PlayerInputMap_COMBAT.OpenSkills.performed -= Combat_OpenSkills_performed;
+            _playerInput.PlayerInputMap_COMBAT.OpenItems.performed -= Combat_OpenItems_performed;
+            _playerInput.PlayerInputMap_COMBAT.Guard.performed -= Combat_Guard_performed;
+            _playerInput.PlayerInputMap_COMBAT.Attack.performed -= Combat_Attack_performed;
+            _playerInput.PlayerInputMap_COMBAT.CycleUp.performed -= Combat_CycleUp_performed;
+            _playerInput.PlayerInputMap_COMBAT.CycleDown.performed -= Combat_CycleDown_performed;
+            _playerInput.PlayerInputMap_COMBAT.CycleLeft.performed -= Combat_CycleLeft_performed;
+            _playerInput.PlayerInputMap_COMBAT.CycleRight.performed -= Combat_CycleRight_performed;
+            _playerInput.PlayerInputMap_COMBAT.Select.performed -= Combat_Select_performed;
+            _playerInput.PlayerInputMap_COMBAT.Back.performed -= Combat_Back_performed;
             _playerInput.PlayerInputMap_EXPLORATION.Disable();
             
             // Exploration Mapping
