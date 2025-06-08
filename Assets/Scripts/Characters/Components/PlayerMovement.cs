ce7f2c6421855bef6ae6226db9207453f17a6941
Chris Liwanag <143138753+cliwanag07@users.noreply.github.com>
2025-06-07 22:35:40 -0700

    PW-82-PW-83: Create proper camera angle (#36)
    * 2d sprite package
    
    * established isometric camera
    
    * player placeholder character
    
    * PW-2-PW-82: Collision & movement system established (#38)
    
    * initial layer collisions established
    
    * initial layer collisions established
    
    * rotated movement vector for isometric view
    
    * made interact collide a trigger
    
    * minor changes
    
    * blocking examples
    
    * made focus text more readable
    
    * positions

--- Assets/Scripts/Characters/Components/PlayerMovement.cs
+++ Assets/Scripts/Characters/Components/PlayerMovement.cs
@@ -21,8 +21,19 @@ namespace FourFatesStudios.ProjectWarden.Characters.Components
     
         private void HandleMovement() {
             var input = _playerController.GetMovementVectorNormalized();
+            input = RotateVector2(input, 45f); // <-- Rotate input 45 degrees clockwise
             var move = new Vector3(input.x, 0f, input.y); // X and Z for movement
             _characterController.Move(move * (moveSpeed * Time.deltaTime));
+        }
+        
+        private static Vector2 RotateVector2(Vector2 v, float degrees) {
+            float radians = degrees * Mathf.Deg2Rad;
+            float sin = Mathf.Sin(radians);
+            float cos = Mathf.Cos(radians);
+            return new Vector2(
+                v.x * cos + v.y * sin,
+                -v.x * sin + v.y * cos
+            );
         }
     }
     
