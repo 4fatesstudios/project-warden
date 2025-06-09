992ba95b80ab0c7e286a7858cec6f4c4ce046065
Chris Liwanag <143138753+cliwanag07@users.noreply.github.com>
2025-06-08 20:50:46 -0700

    added gravity

--- Assets/Scripts/Characters/Components/PlayerMovement.cs
+++ Assets/Scripts/Characters/Components/PlayerMovement.cs
@@ -3,29 +3,42 @@ using FourFatesStudios.ProjectWarden.Characters.Controllers;
 
 namespace FourFatesStudios.ProjectWarden.Characters.Components
 {
-    
     public class PlayerMovement : MonoBehaviour
     {
-        [SerializeField] private float moveSpeed = 5f;
+        [SerializeField] private float moveSpeed = 8f;
+        [SerializeField] private float gravity = -9.81f;
+        [SerializeField] private float groundedGravity = -2f;
+
         private PlayerController _playerController;
         private CharacterController _characterController;
-    
+
+        private float _verticalVelocity;
+
         private void Awake() {
             _playerController = GetComponent<PlayerController>();
             _characterController = GetComponent<CharacterController>();
         }
-    
+
         private void Update() {
             HandleMovement();
         }
-    
+
         private void HandleMovement() {
-            var input = _playerController.GetMovementVectorNormalized();
-            input = RotateVector2(input, 45f); // <-- Rotate input 45 degrees clockwise
-            var move = new Vector3(input.x, 0f, input.y); // X and Z for movement
+            Vector2 input = _playerController.GetMovementVectorNormalized();
+            input = RotateVector2(input, 45f);
+            Vector3 move = new Vector3(input.x, 0f, input.y);
+
+            // Gravity
+            if (_characterController.isGrounded && _verticalVelocity < 0f)
+                _verticalVelocity = groundedGravity;
+            else
+                _verticalVelocity += gravity * Time.deltaTime;
+
+            move.y = _verticalVelocity;
+
             _characterController.Move(move * (moveSpeed * Time.deltaTime));
         }
-        
+
         private static Vector2 RotateVector2(Vector2 v, float degrees) {
             float radians = degrees * Mathf.Deg2Rad;
             float sin = Mathf.Sin(radians);
@@ -36,5 +49,4 @@ namespace FourFatesStudios.ProjectWarden.Characters.Components
             );
         }
     }
-    
 }
