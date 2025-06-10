25a5e799870739e77b1b1e7e9f9ba32642262809
Chris Liwanag <143138753+cliwanag07@users.noreply.github.com>
2025-06-09 23:11:03 -0700

    initial methods

--- Assets/Scripts/Stats/StatModifierManager.cs
+++ Assets/Scripts/Stats/StatModifierManager.cs
@@ -1,10 +1,39 @@
+using System;
 using FourFatesStudios.ProjectWarden.Enums;
+using FourFatesStudios.ProjectWarden.Stats;
 using UnityEngine;
 
 
 namespace FourFatesStudios.ProjectWarden.Characters.Components
 {
-    public class StatModifierManager {
+    public class StatModifierManager : IDisposable {
+        public static Action<StatModifierList> OnStatModifierListAdded;
+        public static Action<StatModifierList> OnStatModifierListRemoved;
+
+        public StatModifierManager() {
+            OnStatModifierListAdded += HandleOnStatModifierListAdded;
+            OnStatModifierListRemoved += HandleOnStatModifierListRemoved;
+        }
+
+        private void HandleOnStatModifierListAdded(StatModifierList obj) {
+            throw new NotImplementedException();
+        }
         
+        private void HandleOnStatModifierListRemoved(StatModifierList obj) {
+            throw new NotImplementedException();
+        }
+
+        public int GetAdditiveModifierValue(Stat stat) {
+            return 0;
+        }
+
+        public int GetMultiplicativeModifierValue(Stat stat) {
+            return 0;
+        }
+
+        public void Dispose() {
+            OnStatModifierListAdded -= HandleOnStatModifierListAdded;
+            OnStatModifierListRemoved -= HandleOnStatModifierListRemoved;
+        }
     }
 }
