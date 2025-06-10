454584beb47ba24b9a0872c8f77db00a90733ab6
Chris Liwanag <143138753+cliwanag07@users.noreply.github.com>
2025-06-09 22:11:40 -0700

    added stat and source properties

--- Assets/Editor/StatModifierDrawer.cs
+++ Assets/Editor/StatModifierDrawer.cs
@@ -6,14 +6,25 @@ using FourFatesStudios.ProjectWarden.ScriptableObjects;
 [CustomPropertyDrawer(typeof(StatModifier))]
 public class StatModifierDrawer : PropertyDrawer
 {
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-    {
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+        SerializedProperty statProp = property.FindPropertyRelative("stat");
         SerializedProperty typeProp = property.FindPropertyRelative("type");
         SerializedProperty valProp = property.FindPropertyRelative("modifier");
+        SerializedProperty sourceProp = property.FindPropertyRelative("source");
 
-        Rect typeRect = new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight);
-        Rect valRect = new Rect(position.x, position.y + EditorGUIUtility.singleLineHeight + 2, position.width, EditorGUIUtility.singleLineHeight);
+        // default source if empty
+        if (string.IsNullOrWhiteSpace(sourceProp.stringValue))
+            sourceProp.stringValue = "Unknown Source";
 
+        float lineHeight = EditorGUIUtility.singleLineHeight;
+        float verticalSpacing = 2f;
+
+        Rect statRect = new Rect(position.x, position.y, position.width, lineHeight);
+        Rect typeRect = new Rect(position.x, statRect.y + lineHeight + verticalSpacing, position.width, lineHeight);
+        Rect valRect = new Rect(position.x, typeRect.y + lineHeight + verticalSpacing, position.width, lineHeight);
+        Rect sourceRect = new Rect(position.x, valRect.y + lineHeight + verticalSpacing, position.width, lineHeight);
+
+        EditorGUI.PropertyField(statRect, statProp);
         EditorGUI.PropertyField(typeRect, typeProp);
 
         StatModifierType modifierType = (StatModifierType)typeProp.enumValueIndex;
@@ -35,13 +46,14 @@ public class StatModifierDrawer : PropertyDrawer
         }
 
         valProp.intValue = Mathf.Clamp(valProp.intValue, min, max);
-
         GUIContent valLabel = new GUIContent("Value", tooltip);
         EditorGUI.IntSlider(valRect, valProp, min, max, valLabel);
+
+        EditorGUI.PropertyField(sourceRect, sourceProp, new GUIContent("Source"));
     }
 
-    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
-    {
-        return EditorGUIUtility.singleLineHeight * 2 + 4;
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
+        // 4 lines total (stat, type, value, source) + spacing
+        return EditorGUIUtility.singleLineHeight * 4 + 6;
     }
 }
--- Assets/Scripts/Stats/StatModifier.cs
+++ Assets/Scripts/Stats/StatModifier.cs
@@ -6,7 +6,9 @@ namespace FourFatesStudios.ProjectWarden.ScriptableObjects
 {
     [System.Serializable]
     public class StatModifier{
+        public Stat stat = Stat.Vitality;
         public StatModifierType type = StatModifierType.Additive;
         public int modifier = 0;
+        public string source = "Unknown Source";
     }
 }
