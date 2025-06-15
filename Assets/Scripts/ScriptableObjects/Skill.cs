cc013cb14c40d5399d3b4b6c3af5ddbe36e516ef
Chris Liwanag <143138753+cliwanag07@users.noreply.github.com>
2025-06-14 19:52:59 -0700

    skill structs

--- Assets/Scripts/ScriptableObjects/Skill.cs
+++ Assets/Scripts/ScriptableObjects/Skill.cs
@@ -1,4 +1,7 @@
+using System;
 using FourFatesStudios.ProjectWarden.Enums;
+using FourFatesStudios.ProjectWarden.Structs;
+using UnityEditor;
 using UnityEngine;
 
 
@@ -6,23 +9,48 @@ namespace FourFatesStudios.ProjectWarden.ScriptableObjects
 {
     [CreateAssetMenu(fileName = "Skill", menuName = "Skill")]
     public class Skill : ScriptableObject {
-        [Header("Damage")]
-        [SerializeField] bool doesDamage;
-        [SerializeField, Tooltip("Base Damage"), Range(1, 9999)] private int baseDamage;
-        [SerializeField, Tooltip("Stagger Damage"), Range(1, 999)] private int stagger;
-        [SerializeField, Tooltip("Exposure Damage"), Range(1, 999)] private int exposure;
-        [SerializeField, Tooltip("Damage Type")] private DamageType damageType;
-        [SerializeField, Tooltip("Aspect")] private Aspect aspect;
+        [SerializeField, Tooltip("The name of the skill")]
+        private string skillName;
+        
+        [SerializeField, Tooltip("The description of the skill")]
+        private string skillDescription;
+        
+        [SerializeField, HideInInspector] private string _skillID;
+
+        [Header("Damage")] 
+        [SerializeField, Tooltip("Damage Instances")] private DamageInstance[] damageInstances;
+        
+        [Header("Healing")]
+        [SerializeField, Tooltip("Damage Instances")] private HealInstance[] healingInstances;
         
         [Header("Buffing")]
-        [SerializeField] bool doesBuffing;
+        [SerializeField] private BuffStatInstance[] buffStatInstances;
+        [SerializeField] private BuffHealInstance[] buffHealInstances;
         
         [Header("Debuffing")]
-        [SerializeField] bool doesDebuffing;
+        [SerializeField] private bool doesDebuffing;
         
         [Header("Common Attributes")]
         [SerializeField, Tooltip("Target Team")] private Team targetTeam;
         [SerializeField, Tooltip("Targeting")] private Targeting targeting;
         [SerializeField, Tooltip("Effect Timing")] private EffectTiming effectTiming;
+        
+#if UNITY_EDITOR
+        [ContextMenu("Regenerate Skill ID")]
+        public void RegenerateSkillID() => _skillID = Guid.NewGuid().ToString();
+        
+        protected virtual void OnValidate()
+        {
+            if (string.IsNullOrEmpty(skillName)) skillName = "Unnamed Skill";
+            if (string.IsNullOrEmpty(skillDescription)) skillDescription = "Empty Description";
+            if (!string.IsNullOrEmpty(_skillID)) return;
+            _skillID = Guid.NewGuid().ToString();
+            EditorUtility.SetDirty(this);
+        }
+#endif
+    }
+
+    public class Damage {
+        
     }
 }
