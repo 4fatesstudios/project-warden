b3f9867734b569312c6ca83f1a1259e4565d0089
Alope396 <Alope396@ucr.edu>
2025-06-29 18:22:05 -0700

    fill

---
+++ Assets/Scripts/Characters/Components/ClassComponent.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+
+namespace FourFatesStudios.ProjectWarden.Characters.Components
+{
+    public class ClassComponent : MonoBehaviour{
+        
+        [SerializeField] private int level;
+        
+        
+    }
+}
