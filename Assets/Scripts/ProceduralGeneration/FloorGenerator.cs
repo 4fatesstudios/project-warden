950e8ebfe5a9c980f536f552d936a89bd3c60f82
Chris Liwanag <143138753+cliwanag07@users.noreply.github.com>
2025-07-02 01:55:09 -0700

    floor generation start

--- Assets/Scripts/ProceduralGeneration/FloorGenerator.cs
+++ Assets/Scripts/ProceduralGeneration/FloorGenerator.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using FourFatesStudios.ProjectWarden.Enums;
+using FourFatesStudios.ProjectWarden.ScriptableObjects.Databases;
 using FourFatesStudios.ProjectWarden.ScriptableObjects.Exploration;
 using UnityEngine;
 
@@ -6,12 +8,21 @@ using UnityEngine;
 namespace FourFatesStudios.ProjectWarden.ProceduralGeneration
 {
     public class FloorGenerator : MonoBehaviour {
+        [SerializeField] private Area area;
         [SerializeField] private int minimumFreeRooms = 8;
         [SerializeField] private int maximumFreeRooms = 12;
         [SerializeField] private List<GameObject> storyPrefabs;
         [SerializeField] private GameObject startingRoom;
+        [SerializeField] private GlobalAreasDatabase globalAreasDatabase;
 
         public void GenerateFloor() {
+            if (!startingRoom) {
+                Debug.Log("Missing starting room");
+                return;
+            }
+            
+            
+            
             Debug.Log("Generating floor");
         }
 
