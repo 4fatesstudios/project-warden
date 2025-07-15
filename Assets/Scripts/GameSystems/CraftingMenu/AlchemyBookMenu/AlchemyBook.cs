317b9dc854c074a90dab9c2bc57e00abed3a4be2
Danny-Vu-Hello <vud20147@gmail.com>
2025-07-14 18:17:42 -0700

    reuse previous commit message here

---
+++ Assets/Scripts/GameSystems/CraftingMenu/AlchemyBookMenu/AlchemyBook.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace FourFatesStudios.ProjectWarden.GameSystems.CraftingMenu.AlchemyBookMenu
+{
+    public class AlchemyBook : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
