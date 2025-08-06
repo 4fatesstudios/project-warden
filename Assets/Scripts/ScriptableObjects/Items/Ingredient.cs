d6d0ab83a503fdd32e6f3d5bf8605977c9509a2c
Chris Liwanag <143138753+cliwanag07@users.noreply.github.com>
2025-08-05 21:21:20 -0700

    Revert "PW-110: Rework Skills and Ingredients (#68)"
    This reverts commit 7b114478d4067e15c894151fefd851c7d09a2ee9.

--- Assets/Scripts/ScriptableObjects/Items/Ingredient.cs
+++ Assets/Scripts/ScriptableObjects/Items/Ingredient.cs
@@ -1,19 +1,13 @@
 using FourFatesStudios.ProjectWarden.Enums;
 using FourFatesStudios.ProjectWarden.ScriptableObjects.PotionEffects;
 using System.Collections.Generic;
-using FourFatesStudios.ProjectWarden.Structs;
 using UnityEngine;
 
 namespace FourFatesStudios.ProjectWarden.ScriptableObjects.Items
 {
     [CreateAssetMenu(fileName = "NewIngredient", menuName = "Items/Ingredient")]
-    public class Ingredient : Item {
-        [SerializeField, Tooltip("Descriptive noun for custom potion naming.")] 
-        private string noun;
-        
-        [SerializeField, Tooltip("Descriptive adjective for custom potion naming.")]
-        private string adjective;
-        
+    public class Ingredient : Item
+    {
         [SerializeField, Tooltip("Alchemical ingredient type.")]
         private IngredientArchetype ingredientArchetype;
 
@@ -23,37 +17,13 @@ namespace FourFatesStudios.ProjectWarden.ScriptableObjects.Items
         [SerializeField, Tooltip("Is alchemical ingredient corrupted or not.")]
         private bool isCorrupted;
 
-        [Header("Ingredient Effect")] 
-        [SerializeField, Tooltip("The infusion(s) given from the ingredient")] 
-        private List<Infusion> infusions;
-
-        [Header("Refinement")] 
-        [SerializeField] private bool canGrind;
-        [SerializeField] private Ingredient grindingResult;
-        
-        [SerializeField] private bool canDistill;
-        [SerializeField] private Ingredient distillingResult;
-        
-        [SerializeField] private bool canRoast;
-        [SerializeField] private Ingredient roastingResult;
-        
-        // DEPRECATED, REMOVE ALL USES, to delete
+        [SerializeField, Tooltip("Potion effects this ingredient can provide.")]
         private List<PotionEffect> potionEffects;
-        public IReadOnlyList<PotionEffect> PotionEffects => potionEffects;
-        // end to delete
 
-        public string Noun => noun;
-        public string Adjective => adjective;
         public IngredientArchetype IngredientArchetype => ingredientArchetype;
         public Aspect IngredientAspect => ingredientAspect;
         public bool IsCorrupted => isCorrupted;
-        public List<Infusion> Infusions => infusions;
-        public bool CanGrind => canGrind;
-        public Ingredient GrindingResult => grindingResult;
-        public bool CanDistill => canDistill;
-        public Ingredient DistillingResult => distillingResult;
-        public bool CanRoast => canRoast;
-        public Ingredient RoastingResult => roastingResult;
+        public IReadOnlyList<PotionEffect> PotionEffects => potionEffects;
 
 #if UNITY_EDITOR
         private void OnValidate()
@@ -65,10 +35,6 @@ namespace FourFatesStudios.ProjectWarden.ScriptableObjects.Items
             {
                 Debug.LogWarning($"[{name}] contains null entries in its PotionEffects list.", this);
             }
-            
-            if (!canGrind) grindingResult = null;
-            if (!canDistill) distillingResult = null;
-            if (!canRoast) roastingResult = null;
         }
 #endif
         public override string ToString()
