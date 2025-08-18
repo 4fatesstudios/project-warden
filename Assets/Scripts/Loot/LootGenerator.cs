c50ca4629fc0a5578b31a0e162b6347ad6386e64
Chris Liwanag <143138753+cliwanag07@users.noreply.github.com>
2025-08-18 10:20:00 -0700

    room design features

--- Assets/Scripts/Loot/LootGenerator.cs
+++ Assets/Scripts/Loot/LootGenerator.cs
@@ -1,5 +1,8 @@
+using FourFatesStudios.ProjectWarden.Enums;
+using FourFatesStudios.ProjectWarden.Interfaces;
 using FourFatesStudios.ProjectWarden.ScriptableObjects.LootTable;
 using UnityEngine;
+using UnityEngine.VFX;
 
 
 namespace FourFatesStudios.ProjectWarden.Loot
@@ -7,7 +10,18 @@ namespace FourFatesStudios.ProjectWarden.Loot
     public class LootGenerator : MonoBehaviour {
         [SerializeField] private LootTable lootTable;
         [SerializeField] private Transform lootGenerationOrigin;
+        [SerializeField] private VisualEffect lootVisualEffect;
+
+        private void OnEnable() {
+            SetLootColorToHighestRarity();
+        }
         
-        
+        private void SetLootColorToHighestRarity() {
+            if (lootVisualEffect.HasVector4("Color")) {
+                lootVisualEffect.SetVector4("Color", RarityColors.Colors[lootTable.GetHighestRarity()]);
+            } else {
+                Debug.Log("Color property not found in LootOrb");
+            }
+        }
     }
 }
